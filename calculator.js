8ae2ac2acb8513e8a9e60d717b4e0d50f21501e1
McNeely <emcneely@ets.org>
2021-03-30 10:49:42 -0500

    final

--- calculator.js
+++ calculator.js
@@ -1,41 +1,48 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById("calc-form");
   if (form) {
     setupIntialValues();
     form.addEventListener("submit", function(e) {
       e.preventDefault();
-      calculateMonthlyPayment();
-      // update();
+      update();
     });
   }
 });
 
+function getCurrentUIValues() {
+  return {
+    amount: +(document.getElementById("loan-amount").value),
+    years: +(document.getElementById("loan-years").value),
+    rate: +(document.getElementById("loan-rate").value),
+  }
+}
+
 function setupIntialValues() {
-  const initLoanAmt = 1000;
-  const initTerm = 10;
-  const initRate = 5;
-
-  const prePopAmt = document.getElementById('loan-amount');
-  prePopAmt.value = initLoanAmt;
-  const prePopTerm = document.getElementById('loan-years');
-  prePopTerm.value = initTerm;
-  const prePopRate = document.getElementById('loan-rate');
-  prePopRate.value = initRate;
-
-  const initMonthlyPmt = ((initLoanAmt * ((initRate / 100) / 12)) / (1 - Math.pow(1 + ((initRate / 100) / 12), -initTerm))).toFixed(2);
-  
-  const prePopPmt = document.getElementById('monthly-payment');
-  prePopPmt.innerText = initMonthlyPmt;
+  const values  = { amount: 10000, years: 10, rate: 4.5 };
+  const amountUI = document.getElementById("loan-amount");
+  amountUI.value = values.amount;
+  const yearsUI = document.getElementById("loan-years");
+  yearsUI.value = values.years;
+  const rateUI = document.getElementById("loan-rate");
+  rateUI.value = values.rate;
+  update();
 }
 
-function calculateMonthlyPayment() {
-  const p = (document.getElementById("loan-amount").value);
-  const n = (document.getElementById("loan-years").value);
-  const i = (document.getElementById("loan-rate").value);
-
-  const monthlyPmt = ((p * ((i/100)/12)) / (1 - Math.pow(1 + ((i/100)/12), -n))).toFixed(2);
-
-  const calculatedPmt = document.getElementById('monthly-payment');
-  calculatedPmt.innerText = monthlyPmt;
+function update() {
+  const currentUIValues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currentUIValues));
 }
 
+function calculateMonthlyPayment(values) {
+  const monthlyRate = (values.rate / 100) / 12;
+  const n = Math.floor(values.years * 12);
+  return (
+    (monthlyRate * values.amount) /
+    (1 - Math.pow((1 + monthlyRate), -n))
+  ).toFixed(2);
+}
+
+function updateMonthly(monthly) {
+  const monthlyUI = document.getElementById("monthly-payment");
+  monthlyUI.innerText = "$" + monthly;
+}
